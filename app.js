27365d4141189a2af4fcf08fe24125136822fbe2
letitia-chiu <hsuan.acc@outlook.com>
2023-10-29 13:18:08 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/todos', (req, res) => {
+  res.send('get all todos')
+})
+
+app.get('/todos/new', (req, res) => {
+  res.send('page: add todos')
+})
+
+app.get('/todos/:id', (req, res) => {
+  res.send(`get todo id: ${req.params.id}`)
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  res.send(`page: edit todo id: ${req.params.id}`)
+})
+
+app.post('/todos', (req, res) => {
+  res.send('add new todo')
+})
+
+app.put('/todos/:id', (req, res) => {
+  res.send(`todo id: ${req.params.id} has been updated`)
+})
+
+app.delete('/todos/:id', (req, res) => {
+  res.send(`todo id: ${req.params.id} has been deleted`)
+})
+
 app.listen(port, () => {
   console.log(`Express server is running on http://localhost:${port}`)
 })
